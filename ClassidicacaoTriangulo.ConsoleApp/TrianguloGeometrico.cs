d2c287a9d27fdfc4562fb9c09cd13741dd73e855
Iagodizlla <iagoschlemper@gmail.com>
2025-03-18 17:03:41 -0300

    Bugs corrigidos, mensagem de aviso criada e adicionado desenhos dos triangulos

---
+++ ClassidicacaoTriangulo.ConsoleApp/TrianguloGeometrico.cs
@@ -0,0 +1,42 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ClassidicacaoTriangulo.ConsoleApp
+{
+    internal class TrianguloGeometrico
+    {
+        public static void GeoEquilatero()
+        {
+            Console.WriteLine("       /\\      ");
+            Console.WriteLine("      /  \\     ");
+            Console.WriteLine("     /    \\    ");
+            Console.WriteLine("    /      \\   ");
+            Console.WriteLine("   /        \\  ");
+            Console.WriteLine("  /          \\ ");
+            Console.WriteLine(" /____________\\");
+        }
+        public static void GeoIsosceles()
+        {
+            Console.WriteLine(" |\\_           ");
+            Console.WriteLine(" |  \\_         ");
+            Console.WriteLine(" |    \\_       ");
+            Console.WriteLine(" |      \\_     ");
+            Console.WriteLine(" |        \\_   ");
+            Console.WriteLine(" |          \\_ ");
+            Console.WriteLine(" |____________\\");
+        }
+        public static void GeoEscaleno()
+        {
+            Console.WriteLine("   |\\_           ");
+            Console.WriteLine("  /   \\_         ");
+            Console.WriteLine("  |     \\_       ");
+            Console.WriteLine(" /        \\_     ");
+            Console.WriteLine(" |          \\_   ");
+            Console.WriteLine("/             \\_ ");
+            Console.WriteLine("|_______________\\");
+        }
+    }
+}
