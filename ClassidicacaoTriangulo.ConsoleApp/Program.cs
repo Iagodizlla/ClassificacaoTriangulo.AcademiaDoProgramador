81df16d7d712e22e4408ad17c33caf469553b893
Iagodizlla <iagoschlemper@gmail.com>
2025-03-18 14:15:10 -0300

    Adicionar arquivos de projeto.

---
+++ ClassidicacaoTriangulo.ConsoleApp/Program.cs
@@ -0,0 +1,66 @@
+﻿namespace ClassidicacaoTriangulo.ConsoleApp
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            while(true) {
+                string tipo;
+                Console.Clear();
+                Console.WriteLine("________________________________");
+                Console.WriteLine("Classificação de Triângulos");
+                Console.WriteLine("--------------------------------");
+                Console.WriteLine("1 - Classificar Triangulo\n2 - Sair");
+                Console.WriteLine("--------------------------------");
+                string op = Console.ReadLine()!;
+
+                if (op == "1")
+                {
+                    Console.Clear();
+                    Console.WriteLine("________________________________");
+                    Console.WriteLine("Classificação de Triângulos");
+                    Console.WriteLine("--------------------------------\n");
+
+                    Console.Write("Digite o valor do lado 1: ");
+                    double lado1 = double.Parse(Console.ReadLine()!);
+                    Console.Write("Digite o valor do lado 2: ");
+                    double lado2 = double.Parse(Console.ReadLine()!);
+                    Console.Write("Digite o valor do lado 3: ");
+                    double lado3 = double.Parse(Console.ReadLine()!);
+
+                    if(lado1 + lado2 > lado3 && lado1 + lado3 > lado2 && lado2 + lado3 > lado1)
+                    {
+                        if (lado1 == lado2 && lado2 == lado3)
+                        {
+                            tipo = "Equilatero";
+                        }
+                        else if (lado1 == lado2 || lado2 == lado3 || lado1 == lado3)
+                        {
+                            tipo = "Isósceles";
+                        }
+                        else
+                        {
+                            tipo = "Escaleno";
+                        }
+                    }
+                    else
+                    {
+                        Console.WriteLine("Não é um triângulo!");
+                        Console.ReadLine();
+                        continue;
+                    }
+                    Console.WriteLine($"Triangulo do tipo: {tipo}");
+                }
+                else if (op == "2")
+                {
+                    break;
+                }
+                else
+                {
+                    Console.WriteLine("Opção inválida!");
+                }
+                Console.ReadLine();
+            }
+        }
+    }
+}
