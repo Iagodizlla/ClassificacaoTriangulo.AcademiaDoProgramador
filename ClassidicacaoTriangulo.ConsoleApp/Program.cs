8761399d38f08c7c29c0080de712f49de89fc015
Iagodizlla <iagoschlemper@gmail.com>
2025-03-18 14:46:43 -0300

    Criado Classe VerificarTipo

--- ClassidicacaoTriangulo.ConsoleApp/Program.cs
+++ ClassidicacaoTriangulo.ConsoleApp/Program.cs
@@ -16,13 +16,13 @@
                     Cabecalho();
                     var (lado1, lado2, lado3) = DadosDosLados();
 
-                    if (TrianguloValido(lado1, lado2, lado3))
+                    if (VerificarTipo.TrianguloValido(lado1, lado2, lado3))
                     {
-                        if (TranguloEquilatero(lado1, lado2, lado3))
+                        if (VerificarTipo.TranguloEquilatero(lado1, lado2, lado3))
                         {
                             tipo = "Equilatero";
                         }
-                        else if (TranguloIsosceles(lado1, lado2, lado3))
+                        else if (VerificarTipo.TranguloIsosceles(lado1, lado2, lado3))
                         {
                             tipo = "Isósceles";
                         }
@@ -33,7 +33,7 @@
                     }
                     else
                     {
-                        TrianguloInvalido();
+                        VerificarTipo.TrianguloInvalido();
                         continue;
                     }
                     Resultado(tipo);
@@ -72,23 +72,6 @@
             Console.Write("Digite o valor do lado 3: ");
             double lado3 = double.Parse(Console.ReadLine()!);
             return (lado1, lado2, lado3);
-        }
-        static bool TrianguloValido(double lado1, double lado2, double lado3)
-        {
-            return lado1 + lado2 > lado3 && lado1 + lado3 > lado2 && lado2 + lado3 > lado1;
-        }
-        static bool TranguloEquilatero(double lado1, double lado2, double lado3)
-        {
-            return lado1 == lado2 && lado2 == lado3;
-        }
-        static bool TranguloIsosceles(double lado1, double lado2, double lado3)
-        {
-            return lado1 == lado2 || lado2 == lado3 || lado1 == lado3;
-        }
-        static void TrianguloInvalido()
-        {
-            Console.WriteLine("Não é um triângulo!");
-            Console.ReadLine();
         }
         static void Resultado(string tipo)
         {
---
+++ ClassidicacaoTriangulo.ConsoleApp/VerificarTipo.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ClassidicacaoTriangulo.ConsoleApp
+{
+    public class VerificarTipo
+    {
+
+        public static bool TranguloEquilatero(double lado1, double lado2, double lado3)
+        {
+            return lado1 == lado2 && lado2 == lado3;
+        }
+        public static bool TranguloIsosceles(double lado1, double lado2, double lado3)
+        {
+            return lado1 == lado2 || lado2 == lado3 || lado1 == lado3;
+        }
+        public static bool TrianguloValido(double lado1, double lado2, double lado3)
+        {
+            return lado1 + lado2 > lado3 && lado1 + lado3 > lado2 && lado2 + lado3 > lado1;
+        }
+        public static void TrianguloInvalido()
+        {
+            Console.WriteLine("Não é um triângulo!");
+            Console.ReadLine();
+        }
+    }
+}
